3307a3fa1a2325f830ffccb7b3a6dfac46a681e3
PlaustralCL <plaustralcl@gmail.com>
2025-07-26 19:38:35 -0400

    Add tokenizer tests for real numbers

--- tests/tokenizertest.pas
+++ tests/tokenizertest.pas
@@ -151,6 +151,62 @@ begin
   end;
 end;
 
+procedure TestRealNum;
+var
+  TestString, msg: string;
+  TokenParser: TTokenizer;
+  ActualTokenList: TStringList;
+  ExpectedTokenList: TStringList;
+  i: integer;
+begin
+  ExpectedTokenList := TStringList.Create;
+  ExpectedTokenList.Add('.111');
+  ExpectedTokenList.Add('*');
+  ExpectedTokenList.Add('22');
+  ExpectedTokenList.Add('/');
+  ExpectedTokenList.Add('.3');
+
+  TestString := '.111 * 22/.3';
+  TokenParser := TTokenizer.Create(TestString);
+  ActualTokenList := TokenParser.ParseTokens;
+  msg := 'Token count for ' + TestString;
+  AssertEqualsInt(ExpectedTokenList.Count, ActualTokenList.Count, msg );
+
+  for i := 0 to ExpectedTokenList.Count - 1 do
+  begin
+    msg := 'Element verification for ' + TestString;
+    AssertEqualsString(ExpectedTokenList[i], ActualTokenList[i], msg);
+  end;
+end;
+
+procedure TestRealNegNum;
+var
+  TestString, msg: string;
+  TokenParser: TTokenizer;
+  ActualTokenList: TStringList;
+  ExpectedTokenList: TStringList;
+  i: integer;
+begin
+  ExpectedTokenList := TStringList.Create;
+  ExpectedTokenList.Add('-.111');
+  ExpectedTokenList.Add('-');
+  ExpectedTokenList.Add('-22');
+  ExpectedTokenList.Add('/');
+  ExpectedTokenList.Add('-.3');
+
+  TestString := '-.111 - -22/-.3';
+  TokenParser := TTokenizer.Create(TestString);
+  ActualTokenList := TokenParser.ParseTokens;
+  msg := 'Token count for ' + TestString;
+  AssertEqualsInt(ExpectedTokenList.Count, ActualTokenList.Count, msg );
+
+  for i := 0 to ExpectedTokenList.Count - 1 do
+  begin
+    msg := 'Element verification for ' + TestString;
+    AssertEqualsString(ExpectedTokenList[i], ActualTokenList[i], msg);
+  end;
+end;
+
 begin
   writeln('');
   writeln('Beginning tests...');
@@ -158,10 +214,11 @@ begin
   TestBasicAdd;
   TestBasicSubtraction;
   TestAddNegativeNumberWithSpace;
+  TestRealNum;
+  TestRealNegNum;
 
   writeln('Tests complete');
   writeln('Number of failed tests: ', FailedTestCount);
   writeln('Number of passed tests: ', PassedTestCount);
-
 end.
 
