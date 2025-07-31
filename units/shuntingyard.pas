b31bb947ada37da4c5d187016d4b809c88fb9714
PlaustralCL <plaustralcl@gmail.com>
2025-07-31 19:36:17 -0400

    [Shunting Yard] First attempt at the shunting yard algoritm
    Attempting algorithm based on https://mathcenter.oxford.emory.edu/site/cs171/shuntingYardAlgorithm/
    Not fully functional.

--- units/shuntingyard.pas
+++ units/shuntingyard.pas
@@ -12,26 +12,31 @@ uses
 
 Type
   TStringStack = specialize TStack<string>;
+  TStringQueue = specialize TQueue<string>;
 
 
   { TShuntingYard }
 
   TShuntingYard = class
+    public
+      constructor Create(TokenList: TStringList);
+      function ConvertToPostfix: TStringList;
+
     private
       //FOperatorStack: TStack;
       FTokenList: TStringList;
       FOperatorStack: TStringStack;
-    public
-      constructor Create(TokenList: TStringList);
-      function ConvertToPostfix: TStringList;
+      FOutputQueue: TStringQueue;
       function IsOperator(token: string): boolean;
+      function IsNumber(token: string): boolean;
+      function IsValidToken(token: string): boolean;
+      function OperatorPrecedence(token: string): integer;
+
+
+
   end;
 
 implementation
-const
-  Operators: array[1..4] of string = ('+', '-', '*', '/');
-
-
 
 
 { TShuntingYard }
@@ -39,10 +44,13 @@ const
 constructor TShuntingYard.Create(TokenList: TStringList);
 begin
   FOperatorStack := TStringStack.Create;
+  FOutputQueue := TStringQueue.Create;
   FTokenList := TokenList;
 end;
 
 function TShuntingYard.IsOperator(token: string): boolean;
+const
+  Operators: array[1..5] of string = ('+', '-', '*', '/', '^');
 var
   i: integer;
 begin
@@ -50,6 +58,36 @@ begin
   for i := low(Operators) to high(operators) do
     if Operators[i] = token then
        IsOperator := True;
+end;
+
+function TShuntingYard.IsNumber(token: string): boolean;
+var
+  f: double;
+begin
+  IsNumber := TryStrToFloat(token, f);
+end;
+
+function TShuntingYard.IsValidToken(token: string): boolean;
+begin
+  Result := False;
+  if (OperatorPrecedence(token) > 0) or
+     IsNumber(token) or
+     (token = '(') or
+     (token = ')') then
+    begin
+      Result := True;
+    end;
+end;
+
+function TShuntingYard.OperatorPrecedence(token: string): integer;
+begin
+  case token of
+    '+', '-': OperatorPrecedence := 1;
+    '*', '/': OperatorPrecedence := 2;
+    '^': OperatorPrecedence := 2;
+    else
+      OperatorPrecedence := -1;
+  end;
 end;
 
 function TShuntingYard.ConvertToPostfix: TStringList;
@@ -61,20 +99,50 @@ begin
   PostFix := TStringList.Create;
   for token in FTokenList do
   begin
-    if IsOperator(token) then
-       FOperatorStack.Push(token);
-    //case token of
-    //'+', '-', '*', '/': FOperatorStack.Push(token);
-    //end;
+    // Source: https://mathcenter.oxford.emory.edu/site/cs171/shuntingYardAlgorithm/
+    // TODO: Need to ensure it is a valid token (number or valid operator or parenthesis)
+    if IsNumber(token) then
+    begin
+      FOutputQueue.Enqueue(token);
+    end
+    else if token = '(' then
+      FOperatorStack.Push(token)
+    else if token = ')' then
+    begin
+      while (FOperatorStack.Count > 0) and (FOperatorStack.Peek <> '(') do
+      begin
+        FOutputQueue.Enqueue(FOperatorStack.Pop);
+      end;
+    end
+    else if (FOperatorStack.Count = 0) or
+            (FOperatorStack.Peek = '(') or
+            (OperatorPrecedence(token) >= OperatorPrecedence(FOperatorStack.Peek)) then
+      FOperatorStack.Push(token)
+    else
+    begin
+      while (FOperatorStack.Count > 0) and (OperatorPrecedence(token) < OperatorPrecedence(FOperatorStack.Peek)) do
+      begin
+        FOutputQueue.Enqueue(FOperatorStack.Pop);
+      end;
+      FOperatorStack.Push(token);
+    end;
   end;
+
   while FOperatorStack.Count > 0 do
   begin
-    PostFix.Add(FOperatorStack.Pop);
+    if FOperatorStack.Peek <> '(' then
+       FOutputQueue.Enqueue(FOperatorStack.Pop)
+    else
+      FOperatorStack.Pop;
 
+  end;
+
+  while FOutputQueue.Count > 0 do
+  begin
+    PostFix.Add(FOutputQueue.Dequeue);
   end;
 
   ConvertToPostfix := PostFix;
-  FOperatorStack.Free;
 
 end;
 
