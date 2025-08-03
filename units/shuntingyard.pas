59bbc339be29cfc03b0fbba0f9df6cfc79aa1984
PlaustralCL <plaustralcl@gmail.com>
2025-08-03 13:46:15 -0400

    [Shunting Yard] Correctly convert to postfix
    Convert the input token list to a postfix string.

--- units/shuntingyard.pas
+++ units/shuntingyard.pas
@@ -23,7 +23,6 @@ Type
       function ConvertToPostfix: TStringList;
 
     private
-      //FOperatorStack: TStack;
       FTokenList: TStringList;
       FOperatorStack: TStringStack;
       FOutputQueue: TStringQueue;
@@ -31,9 +30,6 @@ Type
       function IsNumber(token: string): boolean;
       function IsValidToken(token: string): boolean;
       function OperatorPrecedence(token: string): integer;
-
-
-
   end;
 
 implementation
@@ -82,9 +78,11 @@ end;
 function TShuntingYard.OperatorPrecedence(token: string): integer;
 begin
   case token of
-    '+', '-': OperatorPrecedence := 1;
-    '*', '/': OperatorPrecedence := 2;
-    '^': OperatorPrecedence := 2;
+    '+': OperatorPrecedence := 10;
+    '-': OperatorPrecedence := 10;
+    '*': OperatorPrecedence := 20;
+    '/': OperatorPrecedence := 20;
+    '^': OperatorPrecedence := 30;
     else
       OperatorPrecedence := -1;
   end;
@@ -108,19 +106,34 @@ begin
     else if token = '(' then
       FOperatorStack.Push(token)
     else if token = ')' then
+    { If the incoming symbol is a right parenthesis: discard the right parenthesis,
+    pop stack symbols and add them to the output queue until there is a left parenthesis
+    in the operator stack. Pop the left parenthesis and discard it.}
     begin
       while (FOperatorStack.Count > 0) and (FOperatorStack.Peek <> '(') do
       begin
         FOutputQueue.Enqueue(FOperatorStack.Pop);
       end;
+      if (FOperatorStack.Count > 0) and (FOperatorStack.Peek = '(') then
+      begin
+        FOperatorStack.Pop;
+      end;
     end
     else if (FOperatorStack.Count = 0) or
             (FOperatorStack.Peek = '(') or
-            (OperatorPrecedence(token) >= OperatorPrecedence(FOperatorStack.Peek)) then
+            (OperatorPrecedence(token) > OperatorPrecedence(FOperatorStack.Peek)) then
+    { If the incoming symbol is an operator and has either higher precedence than
+    the operator on the top of the stack, or if the stack is empty, or if the top
+    of the stack is "(" (a floor) -- push it on the stack.}
       FOperatorStack.Push(token)
     else
+    { If the incoming symbol is an operator and has either lower precedence than
+    the operator on the top of the stack, or has the same precedence as the operator
+    on the top of the stack and is left associative -- continue to pop the stack
+    until this is not true. Then, push the incoming operator.
+    }
     begin
-      while (FOperatorStack.Count > 0) and (OperatorPrecedence(token) < OperatorPrecedence(FOperatorStack.Peek)) do
+      while (FOperatorStack.Count > 0) and (OperatorPrecedence(token) <= OperatorPrecedence(FOperatorStack.Peek)) do
       begin
         FOutputQueue.Enqueue(FOperatorStack.Pop);
       end;
@@ -128,22 +141,20 @@ begin
     end;
   end;
 
+  {Pop any remaining operators and add them to the output queue.
+   No parentheses should remain.}
   while FOperatorStack.Count > 0 do
   begin
-    if FOperatorStack.Peek <> '(' then
-       FOutputQueue.Enqueue(FOperatorStack.Pop)
-    else
-      FOperatorStack.Pop;
-
+    FOutputQueue.Enqueue(FOperatorStack.Pop);
   end;
 
+  // Convert the output queue to the TStringList for the return value
   while FOutputQueue.Count > 0 do
   begin
     PostFix.Add(FOutputQueue.Dequeue);
   end;
 
   ConvertToPostfix := PostFix;
-
 end;
 
 end.
